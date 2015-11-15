bf4a1a0ad70f671d97b55bad649f328168472aa9
spencerkerber <spencer.kerber@me.com>
2015-11-15 13:54:11 -0500

    Post Class created

---
+++ Reddit_Application /app/src/main/java/com/spencerkerber/reddit_application/Post.java
@@ -0,0 +1,33 @@
+package com.spencerkerber.reddit_application;
+
+/**
+ * Created by spencerkerber on 11/15/15.
+ */
+public class Post {
+
+    String subreddit;
+    String title;
+    String author;
+    int points;
+    int numComments;
+    String permalink;
+    String url;
+    String domain;
+    String id;
+
+    String getDetails(){
+        String details=author
+                +" posted this and got "
+                +numComments
+                +" replies";
+        return details;
+    }
+
+    String getTitle(){
+        return title;
+    }
+
+    String getScore(){
+        return Integer.toString(points);
+    }
+}
