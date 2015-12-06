29c9530d4a752b213240b63bd6a0887680572fc6
spencerkerber <spencer.kerber@me.com>
2015-12-06 11:47:48 -0500

    Added Parse

--- Reddit_Application /app/src/main/java/com/spencerkerber/reddit_application/MainActivity.java
+++ Reddit_Application /app/src/main/java/com/spencerkerber/reddit_application/MainActivity.java
@@ -3,6 +3,9 @@ package com.spencerkerber.reddit_application;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 /**
  * As of now, all this activity does is create and
  * render a fragment.
@@ -14,6 +17,15 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addFragment();
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "2eowtY2UOx4pXINyumVVjSJoMd5agD25uLDH75sy", "wNsMAL29n7RWOl1uw0EiQ7DVEFCSI4Y59oPNIeGU");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 
     void addFragment(){
